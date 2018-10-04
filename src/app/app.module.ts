a9e7ed0b118869d47af828be5efe4f676231bc57
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-10-04 21:46:26 +0200

    Delete old version of application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    HeaderComponent,
-    FooterComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
